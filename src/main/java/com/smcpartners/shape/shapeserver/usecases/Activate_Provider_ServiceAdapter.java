055430c8dcef8ae0e8a46008795996eaf1af9c03
Bryan Hokanson <hokanson.bryan@gmail.com>
2017-08-03 16:54:20 -0400

    fixed activate provider so it would work

--- src/main/java/com/smcpartners/shape/shapeserver/usecases/Activate_Provider_ServiceAdapter.java
+++ src/main/java/com/smcpartners/shape/shapeserver/usecases/Activate_Provider_ServiceAdapter.java
@@ -3,18 +3,23 @@ package com.smcpartners.shape.shapeserver.usecases;
 
 import com.smcpartners.shape.shapeserver.crosscutting.logging.annotations.Logged;
 import com.smcpartners.shape.shapeserver.crosscutting.security.rest.annotations.Secure;
+import com.smcpartners.shape.shapeserver.frameworks.data.dao.shape.ProviderDAO;
 import com.smcpartners.shape.shapeserver.gateway.rest.services.Activate_Provider_Service;
 import com.smcpartners.shape.shapeserver.shared.constants.SecurityRoleEnum;
 import com.smcpartners.shape.shapeserver.shared.dto.common.BooleanValueDTO;
 import com.smcpartners.shape.shapeserver.shared.dto.common.UserExtras;
+import com.smcpartners.shape.shapeserver.shared.dto.shape.ProviderDTO;
 import com.smcpartners.shape.shapeserver.shared.dto.shape.request.IntEntityIdRequestDTO;
+import com.smcpartners.shape.shapeserver.shared.exceptions.NotAuthorizedToPerformActionException;
 import com.smcpartners.shape.shapeserver.shared.exceptions.UseCaseException;
 
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -34,6 +39,9 @@ public class Activate_Provider_ServiceAdapter implements Activate_Provider_Servi
     @Inject
     UserExtras userExtras;
 
+    @EJB
+    ProviderDAO providerDAO;
+
     public Activate_Provider_ServiceAdapter() {
     }
 
@@ -45,6 +53,32 @@ public class Activate_Provider_ServiceAdapter implements Activate_Provider_Servi
     @Secure({SecurityRoleEnum.ADMIN})
     @Logged
     public BooleanValueDTO activateProvider(IntEntityIdRequestDTO id) throws UseCaseException {
-        return BooleanValueDTO.get(true);
+        try {
+            // Only ADMIN can inactivate any provider
+            // ORG_ADMIN can inactivate org provider
+            if (userExtras.getRole() == SecurityRoleEnum.ADMIN) {
+                providerDAO.changeProviderActiveStatus(id.getEntId(), true);
+            } else {
+                // Is org admin
+                ProviderDTO provider = providerDAO.findById(id.getEntId());
+
+                // Organizations must match
+                if (provider.getOrganizationId() == userExtras.getOrgId()) {
+                    providerDAO.changeProviderActiveStatus(id.getEntId(), true);
+                } else {
+                    throw new NotAuthorizedToPerformActionException();
+                }
+            }
+
+            // Return value
+            return new BooleanValueDTO(true);
+        } catch (Exception e) {
+            log.logp(Level.SEVERE, this.getClass().getName(), "inactivateProvider", e.getMessage(), e);
+            if (e instanceof NotAuthorizedToPerformActionException) {
+                throw (NotAuthorizedToPerformActionException) e;
+            } else {
+                throw new UseCaseException(e.getMessage());
+            }
+        }
     }
 }
